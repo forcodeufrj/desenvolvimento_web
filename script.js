3a5c59317197c2e7bfd016ab2831d12741b607f1
Still-Here-0 <brunovp.21@gmail.com>
2022-04-21 17:27:30 -0300

    .

---
+++ script.js
@@ -0,0 +1,39 @@
+function concent(C1, A1, pH, C2 = '', A2 = '') {
+    atC1 = setC(C1);
+    atC2 = setC(C2);
+    atA1 = setC(A1);
+    atA2 = setC(A2);
+    //equações
+}
+
+function setC(C) {
+    switch (C) {
+        case 'Li' || 'K' || 'Na':
+            break;
+        case 'number':
+            break;
+        case 'boolean':
+            break;
+        default:
+    }
+    return; //at
+}
+
+function setC(A) {
+    switch (A) {
+        case 'Cl':
+            break;
+        case 'K':
+            break;
+        case 'Mg':
+            break;
+        case 'Ca':
+            break;
+        case 'Zn':
+            break;
+        case 'Mg':
+            break;
+        default:
+    }
+    return; //at
+}
